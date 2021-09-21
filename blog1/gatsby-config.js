a695306be12879d4c59743781ed68419e6989924
Chris Wei <yunzhi.wei@live.com>
2021-09-21 04:40:43 +0000

    env: helmet

--- blog1/gatsby-config.js
+++ blog1/gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
   /* Your site config here */
   plugins: [
     'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
 
     {
       resolve: 'gatsby-source-filesystem',
